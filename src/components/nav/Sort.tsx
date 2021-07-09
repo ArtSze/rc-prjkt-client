37c2fc287118a96aca29c5582eb499256784821f
Amanda Pettenati <amanda@pette.net>
2021-07-09 09:57:55 -0400

    add sort select state

--- src/components/nav/Sort.tsx
+++ src/components/nav/Sort.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Select from 'react-select';
+import { useImmer } from 'use-immer';
+import { IOption } from '../../types';
+
+enum SortMethods {
+    'Last Updated' = 'last updated',
+    'Created' = 'created',
+    'Batch' = 'batch',
+}
 
 const Sort = (): JSX.Element => {
-    const options = [
-        { value: 'last updated', label: 'Last Updated' },
-        { value: 'created', label: 'Created' },
-        { value: 'batch', label: 'Batch' },
+    const options: IOption<SortMethods>[] = [
+        { value: SortMethods['Last Updated'], label: 'Last Updated' },
+        { value: SortMethods['Created'], label: 'Created' },
+        { value: SortMethods['Batch'], label: 'Batch' },
     ];
+
+    const [sort, setSort] = useImmer<IOption<SortMethods> | null>(options[2]);
 
     return (
         <div>
             <h3>Sort</h3>
-            <Select options={options} />
+            <Select options={options} value={sort} onChange={setSort} />
         </div>
     );
 };
