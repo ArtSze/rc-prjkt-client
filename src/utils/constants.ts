50a5f58fe1d4e09d4002ad2658768ee3d453315c
Amanda Pettenati <amanda@pette.net>
2021-07-13 09:58:35 -0400

    add axios instance and contansts

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const queryKeys = {
+    users: 'users',
+    tags: 'tags',
+    projects: 'projects',
+    activeProjects: ['projects', 'active'],
+    inactiveProjects: ['projects', 'inactive'],
+    allProjects: ['projects', 'all'],
+};
